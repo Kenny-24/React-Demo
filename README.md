31a1180d8c6be3eff45cb0be38758d2e1e38db48
Kenny <zhangkune24@gmail.com>
2023-01-11 23:10:57 +0800

    feat：webpack初步配置

---
+++ README.md
@@ -0,0 +1,1 @@
+React18 + TS + Webpack
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,5 +1,7 @@
-//借助node服务器serve
-//执行serve -s dist,就可以启动打包后的项目
+// 借助node服务器serve
+// 执行serve -s dist,就可以启动打包后的项目
+// -D后，安装包会在package中的 devDependencies对象中。简称dev。dev是在开发环境中要用到的
+// -S后，安装包会在package中的 dependencies 对象中。简称dep。dep是在生产环境中要用到的
 
 const { merge } = require("webpack-merge");
 const baseConfig = require("./webpack.base.js");
