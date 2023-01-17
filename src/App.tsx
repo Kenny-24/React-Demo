a5e314ae5fb71108a389e84fb4469dff9b9a2977
Kenny <zhangkune24@gmail.com>
2023-01-17 14:16:38 +0800

    feat：处理字体和媒体文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Class from "./components/Class";
-import "./index.css";
+import "./app.css";
+import "./app.less";
 import smallImg from "./assets/imgs/2k.png";
 import bigImg from "./assets/imgs/60k.png";
 
@@ -11,6 +12,8 @@ function App() {
       <Class></Class>
       <img src={smallImg} alt="小于10kb的图片" />
       <img src={bigImg} alt="大于于10kb的图片" />
+      <div className="smallImg"></div> {/* 小图片背景容器 */}
+      <div className="bigImg"></div> {/* 大图片背景容器 */}
     </div>
   );
 }
