88c14594d2589e95b8965c1ca0005bce368fa444
Kenny <zhangkune24@gmail.com>
2023-01-18 21:26:23 +0800

    fix：devtool配置 source map源码映射

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -6,7 +6,8 @@ const baseConfig = require("./webpack.base.js");
 // 合并公共配置,并添加开发环境配置
 module.exports = merge(baseConfig, {
   mode: "development", // 开发模式,打包更加快速,省了代码优化步骤
-  devtool: "eval-cheap-module-source-map", // 源码调试模式
+  devtool: "eval-cheap-module-source-map",
+  // 源码调试模式，eval 缓存的原因, 热更新会很快；cheap 只需要定位到行信息,不需要列信息；module 展示源码错误信息
   devServer: {
     port: 8666, // 服务端口号
     compress: false, // gzip压缩,开发环境不开启,提升热更新速度
